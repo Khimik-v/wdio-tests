6d7f9c9ecc6d44e2a482681b0142a4f8f0345352
Khimik-v <152432965+Khimik-v@users.noreply.github.com>
2026-06-09 15:55:04 +0200

    first commit

---
+++ test/specs/example.js
@@ -0,0 +1,12 @@
+browser.url('https://webdriver.io');
+browser.pause();
+browser.getTitle();
+
+$("locator")
+addValue();
+getValue();
+setValue();
+click();
+getAttribute();
+getLocation();
+getText();
